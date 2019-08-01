899d2a0b99717b11c09f782c137e9e09bee925ee
Ben Parker <bpar0005@student.monash.edu>
2019-08-01 11:11:53 +0200

    Update + Delete task ready to test.

--- src/task-manager/task-manager.service.ts
+++ src/task-manager/task-manager.service.ts
@@ -3,29 +3,37 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Tasks } from '../entities/tasks.entity';
 import { Repository } from 'typeorm';
 import { TaskDTO } from './task.dto';
+import { TaskManager } from '../entities/task-manager.entity';
 
 @Injectable()
 export class TaskManagerService {
-    constructor(
+  constructor(
     @InjectRepository(Tasks)
     private tasksRepository: Repository<Tasks>,
-    ) {}
+    @InjectRepository(TaskManager)
+    private taskManagerRepository: Repository<TaskManager>,
+  ) {}
 
-    async fetchUserTasks(userId: number): Promise<any> {
+    async fetchUserTasks(userId: number): Promise<Tasks[]> {
       // Get list of taskIds from task manager where userId = userID
+      const taskIds = number[];
+      await this.taskManagerRepository.find(userId);
       // Use list of taskIds to fetch task details
       return await this.tasksRepository.find(userId);
     }
 
-    async addTask(userId: number, Task: TaskDTO): Promise<any> {
+    async addTask(userId: number, taskDTO: TaskDTO): Promise<any> {
       // Use task details to create task, and get its ID
       // Insert task ID and userId into task manager
       return await this.tasksRepository.insert(TaskDTO);
     }
 
-    async updateTask(taskId: number, Task: TaskDTO): Promise<any> {
-      // Update the task with new details, based on the taskId
-      return await this.tasksRepository.update(taskId, Task);
+    async updateTask(taskId: number, taskDTO: TaskDTO): Promise<any> {
+      // Find the task to update, using taskId
+      const task = await this.tasksRepository.findOne(taskId);
+      // Update the task details, using the taskDTO
+      const updatedTask = Object.assign(task, taskDTO);
+      return await this.tasksRepository.save(updatedTask);
     }
 
     async removeTask(taskId: number): Promise<any> {
