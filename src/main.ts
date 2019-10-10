d5ceda1c640b10c52246ca31f0786d0d72991d3f
Andrew Lyons <alyo0002@student.monash.edu>
2019-10-10 14:55:46 +0200

    heroku build setup

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
-  await app.listen(4000);
+  await app.listen(process.env.PORT || 4000);
 }
 
 bootstrap();
